35b976612bbe0f61da36804144d8336ef23d8973
Cristhian Pineda <cristhianpinedacely@gmail.com>
2020-07-19 01:10:12 -0400

    Spring Boot.
    Spring Boot

---
+++ co-ops-api/src/main/java/amida/cristhian/coopsapi/CoOpsApiApplication.java
@@ -0,0 +1,13 @@
+package amida.cristhian.coopsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoOpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoOpsApiApplication.class, args);
+	}
+
+}
