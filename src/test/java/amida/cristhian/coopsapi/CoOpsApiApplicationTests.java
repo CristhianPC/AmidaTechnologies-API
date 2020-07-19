5e74655770876832a5fba440a7159771051b91f7
Cristhian Pineda <cristhianpinedacely@gmail.com>
2020-07-19 00:18:46 -0400

    Spring Boot API
    Spring Boot API

---
+++ src/test/java/amida/cristhian/coopsapi/CoOpsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package amida.cristhian.coopsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoOpsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
